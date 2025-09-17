8f61bd46c0b189832d61983ca7cb76297fd270f8
Joe Wallwork <jw2423@cam.ac.uk>
2025-09-17 08:51:20 +0100

    Progress plotting for two-parameter case

---
+++ experiments/box_4turbine_2param/plot_progress.py
@@ -0,0 +1,52 @@
+"""Plot progress of controls, QoIs, gradients, and DoFs on different axes.
+
+Run `python3 plot_progress.py --help` to see the available command-line options.
+"""
+
+import argparse
+import os
+
+import matplotlib.pyplot as plt
+from setup import TwoParameterSetup
+
+from turbine_opt_adapt.experiment import get_latest_experiment_id
+from turbine_opt_adapt.plotting import ProgressPlotter
+
+parser = argparse.ArgumentParser(
+    description="Plot progress of controls and QoIs on different axes."
+)
+parser.add_argument("--n", type=int, default=0, help="Initial mesh resolution.")
+parser.add_argument(
+    "--git_hash", type=str, default=None, help="Git hash identifier for the experiment."
+)
+args = parser.parse_args()
+targets = [1000, 2000, 4000]
+n_range = [0, 1, 2, 2.5850]
+experiment_id = get_latest_experiment_id(git_hash=args.git_hash)
+plot_dir = f"plots/{experiment_id}"
+if not os.path.exists(plot_dir):
+    os.makedirs(plot_dir)
+
+for i, label in enumerate(("qois", "dofs")):
+    fig, axes = plt.subplots()
+    plotter = ProgressPlotter(
+        TwoParameterSetup,
+        axes,
+        "timings",
+        label,
+        experiment_id,
+        args.n,
+        n_range,
+        targets,
+    )
+    plotter.plot_all()
+    handles, labels = axes.get_legend_handles_labels()
+    plt.savefig(f"{plot_dir}/{label}.jpg", bbox_inches="tight")
+
+    if i == 0:
+        # Save the legend separately
+        fig = plt.figure()
+        fig.legend(handles, labels, loc="center", frameon=False, ncol=3)
+        fig.savefig(f"{plot_dir}/legend.jpg", bbox_inches="tight")
+
+# TODO: Plot controls and gradients on the 2D parameter space
